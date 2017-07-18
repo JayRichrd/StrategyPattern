d165b2915f046928df168636855acdc90c21aeb8
jiangyu <jiangyu@yulong.com>
2017-07-18 14:31:01 +0800

    使用简单工厂模式改进

--- src/com/jy/CashContext.java
+++ src/com/jy/CashContext.java
@@ -7,12 +7,40 @@ package com.jy;
  *
  */
 public class CashContext {
+	// 不同的具体策略算法类
+	private static final int CASH_NORMAL = 0x1;
+	private static final int CASH_DISCOUNT = 0x2;
+	private static final int CASH_RETURN = 0x3;
 	// 抽象策略
 	private CashSuper mCashSuper;
 
-	public CashContext(CashSuper mCashSuper) {
+	/**
+	 * 带参数的构造函数
+	 * 
+	 * @param type
+	 *            活动类型
+	 * @param rateDiscount
+	 *            打折比例
+	 * @param moneyCondition
+	 *            返现活动满额
+	 * @param moneyReturn
+	 *            返现活动返额
+	 */
+	public CashContext(int type, double rateDiscount, int moneyCondition, int moneyReturn) {
 		super();
-		this.mCashSuper = mCashSuper;
+		switch (type) {
+		case CASH_NORMAL: // 正常情况
+			mCashSuper = new CashNormal();
+			break;
+		case CASH_DISCOUNT: // 打折活动
+			mCashSuper = new CashDiscount(rateDiscount);
+			break;
+		case CASH_RETURN: // 返现活动
+			mCashSuper = new CashReturn(moneyCondition, moneyReturn);
+			break;
+		default:
+			break;
+		}
 	}
 
 	/**
--- src/com/jy/StrategyPattern.java
+++ src/com/jy/StrategyPattern.java
@@ -1,19 +1,26 @@
 package com.jy;
 
 public class StrategyPattern {
+	// 不同的具体策略算法类
+	private static final int CASH_NORMAL = 0x1;
+	private static final int CASH_DISCOUNT = 0x2;
+	private static final int CASH_RETURN = 0x3;
 
 	public static void main(String[] args) {
 		CashContext cashContext = null;
-		// 原价
 		double inputCost = 500.0;
 
+		// 原价
+		cashContext = new CashContext(CASH_NORMAL, 0, 0, 0);
+		System.out.println("原价:￥" + inputCost + ";" + "折扣后应收:￥" + cashContext.getResult(inputCost));
+
 		// 打折
-		cashContext = new CashContext(new CashDiscount(0.8));
-		System.out.println("原价:￥" + inputCost + ";" + "打折后应收:￥" + cashContext.getResult(inputCost));
+		cashContext = new CashContext(CASH_DISCOUNT, 0.8, 0, 0);
+		System.out.println("原价:￥" + inputCost + ";" + "折扣后应收:￥" + cashContext.getResult(inputCost));
 
 		// 满减
-		cashContext = new CashContext(new CashReturn(200, 100));
-		System.out.println("原价:￥" + inputCost + ";" + "满减后应收:￥" + cashContext.getResult(inputCost));
+		cashContext = new CashContext(CASH_RETURN, 0, 200, 100);
+		System.out.println("原价:￥" + inputCost + ";" + "折扣后应收:￥" + cashContext.getResult(inputCost));
 
 	}
 
